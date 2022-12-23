a7610a527576f8d4bf73bd79dcb92f310061c7d9
DanielaIH <4427@holbertonschool.com>
2022-12-22 23:53:19 -0500

    ADD task 1

---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * heap_insert - insert a node into the max heap.
+ * @root: the root node.
+ * @value: the value to insert.
+ * Return: the node inserted or null.
+ */
+heap_t *heap_insert(heap_t **root, int value)
+{
+    heap_t *node = NULL;
+
+    if (!root)
+		return (NULL);
+
+    node = malloc(sizeof(heap_t));
+
+    if (!node)
+		return (NULL);
+
+	node = binary_tree_node(NULL, value);
+
+	if (!*root)
+	{
+		*root = node;
+		return (node);
+	}
+
+    return(NULL);
+}
