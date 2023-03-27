1effce7b7fcc7a8b56439a891ef2fc849364910d
DanielaIH <4427@holbertonschool.com>
2023-03-26 21:20:13 -0500

    ADD lockboxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""method that determines if all the boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+    """method that determines if all the boxes can be opened"""
+    global keys
+    keys = []
+    if boxes:
+        callback(boxes, 0)
+    else:
+        return False
+
+    if len(boxes) == max(keys) + 1:
+        return True
+    else:
+        return False
+
+
+def callback(boxes, index):
+    """ recursive function to call itself"""
+    try:
+        for key in boxes[index]:
+            if key not in keys:
+                keys.append(key)
+                callback(boxes, key)
+    except:
+        pass
