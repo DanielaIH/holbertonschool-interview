7e7e96e8b9688475865c58dc7522722c68268391
DanielaIH <4427@holbertonschool.com>
2023-03-31 23:36:38 -0500

    ADD task

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,6 +1,20 @@
 #include "binary_trees.h"
 
 /**
+ * length - find the length of the binary tree.
+ * @root: the root node.
+ * Return: the length of the binary tree.
+ */
+int length(heap_t **root)
+{
+	if (!root || !*root)
+		return (0);
+
+	return (length(&((*root)->left)) + length(&((*root)->right)) + 1);
+}
+
+
+/**
  * heap_insert - insert a node into the max heap.
  * @root: the root node.
  * @value: the value to insert.
@@ -9,6 +23,7 @@
 heap_t *heap_insert(heap_t **root, int value)
 {
 	heap_t *node = NULL;
+	int len = (length(root) - 1) / 2;
 
 	if (!root)
 		return (NULL);
@@ -25,6 +40,16 @@ heap_t *heap_insert(heap_t **root, int value)
 		*root = node;
 		return (node);
 	}
-
+	if (len == 0)
+		return (NULL);
+	if (len > 0 && len % 2)
+		(*root) = (*root)->left;
+	if (len > 0 && !(len % 2))
+		*root = (*root)->right;
+	node->(*root) = *root;
+	if (!(*root)->left)
+		(*root)->left = node;
+	else
+		(*root)->right = node;
 	return (node);
 }
